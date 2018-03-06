7b558134c681edb65e392ff9d63ca8cdaac61678
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-03-06 13:42:19 +0100

    Add home page

--- src/main/java/com/example/myspringbootapp20180306/DemoApplication.java
+++ src/main/java/com/example/myspringbootapp20180306/DemoApplication.java
@@ -2,11 +2,22 @@ package com.example.myspringbootapp20180306;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	@ResponseBody
+	public String home(){
+		return "Hello world";
 	}
 }
